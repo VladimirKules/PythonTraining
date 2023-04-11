03ed796b874300eeb71baf8a592a5629ede651c3
VladimirKules <130364861+VladimirKules@users.noreply.github.com>
2023-04-11 20:15:50 +0300

    Add files via upload

---
+++ Lesson2/step2.py
@@ -0,0 +1,4 @@
+from transliterate import translit
+print(translit("Lorem ipsum dolor sit amet", 'hy'))  
+print(translit("Lorem ipsum dolor sit amet", 'ka')) 
+print(translit("Lorem ipsum dolor sit amet", 'uk'))
---
+++ Lesson2/step4.py
@@ -0,0 +1,4 @@
+from num2words import num2words
+print(num2words(42))  
+print(num2words(42, to='ordinal')) 
+print(num2words(42, lang='fr'))  
